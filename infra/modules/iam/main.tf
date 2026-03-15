bf9a287b9b7a03df6a120ea4ac036b2a5891753f
Ayub Farah <ayubfarahh@gmail.com>
2026-03-15 05:32:15 +0000

    created the trust policy for github actions oidc

--- infra/modules/iam/main.tf
+++ infra/modules/iam/main.tf
@@ -1,3 +1,4 @@
+## CODEDEPLOY PERMS
 data "aws_iam_policy_document" "code_deploy_policy_role" {
   statement {
     actions = ["sts:AssumeRole"]
@@ -20,6 +21,7 @@ resource "aws_iam_role_policy_attachment" "code_deploy_policy_role_attachment" {
   policy_arn = "arn:aws:iam::aws:policy/AWSCodeDeployRoleForECS"
 }
 
+## ECS PERMS
 data "aws_iam_policy_document" "ecs_tasks_execution_role" {
   statement {
     actions = ["sts:AssumeRole"]
@@ -41,7 +43,7 @@ resource "aws_iam_role_policy_attachment" "ecs_tasks_execution_role" {
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
 
-## 
+
 data "aws_iam_policy_document" "task_role_dynamodb" {
   statement {
     effect = "Allow"
@@ -91,4 +93,43 @@ resource "aws_iam_role_policy_attachment" "task_role_policy_attachment" {
   policy_arn = aws_iam_policy.task_role_policy.arn
   role      = aws_iam_role.task_role.name
   
+}
+
+## OIDC for github actions
+
+resource "aws_iam_openid_connect_provider" "github_actions" {
+  url = "https://token.actions.githubusercontent.com"
+  client_id_list = [ "sts.amazonaws.com" ]
+  thumbprint_list = [ "6938fd4d98bab03faadb97b34396831e3780aea1" ]
+
+}
+
+data "aws_iam_policy_document" "github_actions_assume_role" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Federated"
+      identifiers = [aws_iam_openid_connect_provider.github_actions.arn]
+    }
+
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+
+    condition {
+      test     = "StringEquals"
+      variable = "token.actions.githubusercontent.com:aud"
+      values   = ["sts.amazonaws.com"]
+    }
+
+    condition {
+      test     = "StringEquals"
+      variable = "token.actions.githubusercontent.com:sub"
+      values   = ["repo:ayubfarahh/ECS-2.0:ref:refs/heads/main"]
+    }
+  }
+}
+
+resource "aws_iam_role" "github_actions_role" {
+  name               = "github-actions-role"
+  assume_role_policy = data.aws_iam_policy_document.github_actions_assume_role.json
 }
